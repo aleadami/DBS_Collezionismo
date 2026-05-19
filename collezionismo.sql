f2614a898a0bd498ea9c994573bdc38c9a323c57
aleadami <alessandro.adami2003@gmail.com>
2026-05-19 12:33:45 +0200

    aggiornato sql

--- collezionismo.sql
+++ collezionismo.sql
@@ -2,7 +2,9 @@
 CREATE TABLE Utente (
     Nickname VARCHAR(10) PRIMARY KEY,
     Email VARCHAR(20),
-    Data_Iscrizione DATE
+    Data_Iscrizione DATE,
+    FOREIGN KEY (Sede) REFERENCES Sede_Legale(Nome_Piattaforma),
+    FOREIGN KEY (Mazzo) REFERENCES Mazzo(Codice_Mazzo)
 );
 
 --sottotipo di Utente
@@ -11,6 +13,7 @@ CREATE TABLE Solitario (
     Utente VARCHAR() PRIMARY KEY,
     Nome VARCHAR(),
     Cognome VARCHAR()
+    FOREIGN KEY (Biglietto) REFERENCES Biglietto(Codice_Seriale)
 );
 
 --sottotipo di Utente
@@ -19,13 +22,16 @@ CREATE TABLE Squadra (
     Utente VARCHAR() PRIMARY KEY,
     Tag_Squadra VARCHAR(),
     Numero_Membri VARCHAR()
+    FOREIGN KEY (Esports) REFERENCES Organizzazione_Esports(Partita_IVA)
 );
 
---
+--legata a Espansione, Restrizione, Utente, Mazzo
 CREATE TABLE Carta (
     Codice_Carta VARCHAR(10) PRIMARY KEY,
     Nome VARCHAR(20),
     Testo_Descrizione TEXT
+    FOREIGN KEY (Espansione) REFERENCES Espansione(Codice_Espansione)
+    FOREIGN KEY (Restrizione) REFERENCES Restrizione(Nome_Lista)
 );
 
 --sottotipo di Carta
@@ -44,7 +50,7 @@ CREATE TABLE Pokemon (
 CREATE TABLE Aiuto (
     Carta VARCHAR(8) PRIMARY KEY,
     Effetto TEXT,
-    Sottotipo VARCHAR
+    Sottotipo VARCHAR()
 );
 
 --legata a Pokemon
@@ -60,12 +66,20 @@ CREATE TABLE Meccanica_Torneo (
     Limite_Nel_Mazzo INT,
     --serve per dire dove va la carta dopo che ha usato quella meccanica
     --Zona_Perduta VARCHAR()
+    FOREIGN KEY (Aiuto) REFERENCES Aiuto(Carta)
 );
 
 --legato a Carta
 CREATE TABLE Espansione (
     Codice_Espansione VARCHAR(6) PRIMARY KEY,
-    Nome_Espansione VARCHAR(),
+    Nome_Espansione VARCHAR()
+);
+
+--legato a Carta
+CREATE TABLE Restrizione (
+    Nome_Lista VARCHAR(8) PRIMARY KEY,
+    Limitazione INT,
+    Bannato VARCHAR()
 );
 
 --legato a Utente
@@ -87,13 +101,6 @@ CREATE TABLE Digitale (
     Account_Torneo VARCHAR()
 );
 
---legato a Carta
-CREATE TABLE Restrizione (
-    Nome_Lista VARCHAR(8) PRIMARY KEY,
-    Limitazione INT,
-    Bannato VARCHAR()
-);
-
 --legata a Carta, Utente
 CREATE TABLE Mazzo (
     Codice_Mazzo VARCHAR(8) PRIMARY KEY,
@@ -103,28 +110,35 @@ CREATE TABLE Mazzo (
 
 --legato a Utente e Risultato
 CREATE TABLE Risultato (
-    Codice_Risultato VARCHAR(),
+    Utente VARCHAR(),
+    Partita VARCHAR(),
+    --ha senso metterlo ma in questo modo non è più un'entità debole
+    --Codice_Risultato VARCHAR(),
     Punteggio VARCHAR(3),
     Bonus_Assegnati TEXT,
-    Penalità_Assegante TEXT
+    Penalità_Assegante TEXT,
+    PRIMARY KEY (Utente, Partita, Punteggio)
+    FOREIGN KEY (Utente) REFERENCES Utente(Nickname)
 );
 
 --legato a Risultato
 CREATE TABLE Partita (
     Codice_Partita VARCHAR()
     Ora_Inizio TIMESTAMP,
-    Fase_Torneo VARCHAR()
+    Fase_Torneo VARCHAR(),
+    FOREIGN KEY (Risultato) REFERENCES Risultato(Codice_Risultato)
 );
 
 --legato a Singolo
 CREATE TABLE Biglietto (
-    Codice_Seriale VARCHAR(),
+    Codice_Seriale VARCHAR() PRIMARY KEY,
     Prezzo DECIMAL
 );
 
 --legato a Squadra
 CREATE TABLE Organizzazione_Esports (
-    Partita_IVA VARCHAR(),
+    Partita_IVA VARCHAR() PRIMARY KEY,
     Sede_Legale VARCHAR(),
     Budget_Sponsorizzazione DECIMAL
+    FOREIGN KEY (Squadra) REFERENCES Squadra(Nickname)
 );
